a925fd98435c7f1d29d4b2c09ad49643d57a3c06
Saul Sayers <saulsayers@gmail.com>
2022-04-20 05:25:43 +0700

    dummy booyermore

--- src/main/algorithm.go
+++ src/main/algorithm.go
@@ -1,21 +1,21 @@
 package main
 
-// import (
-// 	"fmt"
-// )
+import (
+	"fmt"
+)
 
 func kmpBorderFunc(pola string) []int {
 	// Border function dari KMP merupakan array yang berisi panjang prefix maksimal yang sama dengan sufix untuk tiap k nya
 
 	// KAMUS
 	i := 0 // Menunjuk ke huruf terakhir pada prefix yang cocok dengan sufix untuk suatu k
-	runePola := []rune(pola) // Konversi string menjadi array of char agar bisa mengakses tiap char
+	runePola := []rune(pola) // Konversi string menjadi array of rune agar bisa mengakses tiap char
 	panjangPola := len(runePola)
 	var border = make([]int,panjangPola-1) // Menggunakan slice karena array pada golang tidak bisa ngefix sizenya dari variabel
 	
 	// ALGORITMA
 	for k := 1 ; k < len(border); k++ {
-		for (runePola[k] != runePola[i] && i != 0) { // Pencarian i apabila huruf terakhirnya tidak sama
+		for (runePola[k] != runePola[i] && i != 0) { // Pencarian i terlebih dahulu apabila huruf terakhirnya tidak sama
 			i = border[i-1] // Dengan cara memperkecil i hingga ditemukan pasangan prefix-sufix yang cocok atau hingga habis tidak ditemukan
 		}
 		if runePola[k] == runePola[i]{ // Kasus pencarian i menemukan pasangan cocok, maka border function di k adalah panjang dari prefix
@@ -35,7 +35,7 @@ func kmp(dna string, pola string) bool{
 	// KAMUS
 	i := 0 // merujuk ke huruf pada DNA yang sedang diperiksa
 	j := 0 // merujuk ke huruf pada pola yang sedang diperiksa
-	runeDNA := []rune(dna) // Konversi string menjadi array of char agar bisa mengakses tiap char
+	runeDNA := []rune(dna) // Konversi string menjadi array of rune agar bisa mengakses tiap char
 	runePola := []rune(pola) 
 	panjangDNA := len(runeDNA) 
 	panjangPola := len(runePola)
@@ -64,7 +64,34 @@ func kmp(dna string, pola string) bool{
 	return found
 }
 
-// func main() {
+func booyermoreLastIndex(dna string, pola string) map[string]int {
+	// KAMUS
+	last := make(map[string]int) // Menggunakan map untuk menunjukkan lokasi huruf terakhirnya
+	runePola := []rune(pola) // Konversi string menjadi array of char agar bisa mengakses tiap char
+	runeDNA := []rune(dna)
+	panjangPola := len(runePola)
+	panjangDNA := len(dna)
+
+	// ALGORITMA
+	for i := 0; i < panjangDNA ; i++ { // Menginisiasi tiap huruf dalam DNA sebagai -1 terlebih dahulu
+		last[string(runeDNA[i])] = -1
+	}
+
+	for i := 0; i < panjangPola ; i++ { //Mengisi index terakhir sebuah char ditemukan dalam pola
+		last[string(runePola[i])] = i
+	}
+
+	return last
+}
+
+func booyermore(dna string, pola string) bool{
+	// KAMUS
+
+	// ALGORITMA
+	return false;
+}
+
+//func main() {
 	// TES SINTAKS DASAR
 	// kalimat := "IPnya kiky adalah "
 	// IPK := 4
@@ -81,4 +108,8 @@ func kmp(dna string, pola string) bool{
 	// TES KMP 
 	// fmt.Print(kmp("abcabsda","bsda"))
 
-// }
+	// TEST BOOYERMOORE LAST INDEX FUNCTION
+	// last := booyermoreLastIndex("abcd","abacab")
+	// fmt.Print(last)
+
+//}
